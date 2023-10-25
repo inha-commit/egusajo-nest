218407ba543a4e31828d4f27adfec559914e1e62
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-10-25 14:11:16 +0900

    Delete un-used vars

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Logger, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { UserImageEntity } from '../entities/userImage.entity';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
