53a7a674e453ab1e0e9c2f5192b826a5743ba6b5
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-10-29 18:05:28 +0900

    Add validation schema

--- src/config/validationSchema.ts
+++ src/config/validationSchema.ts
@@ -15,4 +15,9 @@ export const validationSchema = Joi.object({
   S3_SECRET_KEY: Joi.string().required(),
   S3_BUCKET_NAME: Joi.string().required(),
   S3_REGION: Joi.string().required(),
+
+  SLACK_BOT_TOKEN: Joi.string().required(),
+  SLACK_NEW_USER_CHANNEL_ID: Joi.string().required(),
+  SLACK_API_LATENCY_CHANNEL_ID: Joi.string().required(),
+  SLACK_FATAL_ERROR_CHANNEL_ID: Joi.string().required(),
 });
